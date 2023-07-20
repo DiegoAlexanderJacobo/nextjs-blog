3d6d31a360df6864c0ce7d3be6412c1439de246f
Diego Jacobo <djacobo@quanticotrends.com>
2023-07-20 13:01:03 -0500

    15

--- pages/blog/comps/ItemBlog.jsx
+++ pages/blog/comps/ItemBlog.jsx
@@ -10,7 +10,7 @@ export default function ItemBlog({dataPost}) {
 
   return (
     <div className={styles.boxDataItem}>
-      {console.log(dataPost.id)}
+      {console.log(dataPost)}
       <Link href={"/blog/2"}>
         <div className={styles.itemContainer}>
             <h3 className={styles.itemTitle}>2eddssdsdsdsdssdfer</h3>
