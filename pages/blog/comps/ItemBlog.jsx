0fe4233593d6b77a11da34ec190819287c4b8e1d
Diego Jacobo <djacobo@quanticotrends.com>
2023-07-20 11:55:02 -0500

    s3

--- pages/blog/comps/ItemBlog.jsx
+++ pages/blog/comps/ItemBlog.jsx
@@ -4,7 +4,7 @@ import styles from '../../../styles/StylePath.module.css'
 export default function ItemBlog({dataPost}) {
   return (
     <div className={styles.boxDataItem}>
-      <Link href={`/blog/1`}>
+      <Link href={`/blog/3`}>
         <div className={styles.itemContainer}>
             <h3 className={styles.itemTitle}>{dataPost.title.toUpperCase()}</h3>
         </div>
