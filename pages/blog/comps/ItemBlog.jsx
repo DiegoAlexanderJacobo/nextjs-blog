589f791abb61a2622d7a797c22920eddf82aa2b2
Diego Jacobo <djacobo@quanticotrends.com>
2023-07-20 12:20:58 -0500

    s8

--- pages/blog/comps/ItemBlog.jsx
+++ pages/blog/comps/ItemBlog.jsx
@@ -4,7 +4,7 @@ import styles from '../../../styles/StylePath.module.css'
 export default function ItemBlog({dataPost}) {
   return (
     <div className={styles.boxDataItem}>
-      <Link href={`/blog/${dataPost[id]}`}>
+      <Link href={`/blog/${dataPost['id']}`}>
         <div className={styles.itemContainer}>
             <h3 className={styles.itemTitle}>2efer</h3>
         </div>
