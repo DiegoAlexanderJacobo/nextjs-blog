057d4014a4ab3145b8263742e6d036999f5cb2b1
Diego Jacobo <djacobo@quanticotrends.com>
2023-07-20 13:12:09 -0500

    s19

--- pages/blog/comps/ItemBlog.jsx
+++ pages/blog/comps/ItemBlog.jsx
@@ -2,17 +2,19 @@ import Link from 'next/link'
 import styles from '../../../styles/StylePath.module.css'
 
 export default function ItemBlog({dataPost}) {
-  try {
-    const dataId = dataPost.id;
-    const dataTitle = dataPost.title;
-  } catch (error) {
-    console.log("Error en datapost")
+  
+  const dataIdTitle = () => {
+    try {
+      return [dataPost.id, dataPost.title];
+    } catch (error) {
+      console.log("Error en datapost: " + error)
+    }
   }
 
   return (
     <div className={styles.boxDataItem}>
-      {console.log(dataId)}
-      <Link href={"/blog/2"}>
+      {console.log(dataIdTitle())}
+      <Link href={`/blog/${dataId}`}>
         <div className={styles.itemContainer}>
             <h3 className={styles.itemTitle}>2eddssdsdsdsdssdfer</h3>
         </div>
