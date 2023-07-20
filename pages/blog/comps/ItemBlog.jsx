5dcb272cf5cd254ad34bfe12717a8d8b8114ddd9
Diego Jacobo <djacobo@quanticotrends.com>
2023-07-20 12:30:52 -0500

    s11

--- pages/blog/comps/ItemBlog.jsx
+++ pages/blog/comps/ItemBlog.jsx
@@ -2,9 +2,14 @@ import Link from 'next/link'
 import styles from '../../../styles/StylePath.module.css'
 
 export default function ItemBlog({dataPost}) {
+  try {
+    console.log(dataPost.id);
+  } catch (error) {
+    console.log("error en datapost")
+  }
+
   return (
     <div className={styles.boxDataItem}>
-      {console.log(dataPost.id)}
       <Link href={"/blog/2"}>
         <div className={styles.itemContainer}>
             <h3 className={styles.itemTitle}>2efer</h3>
